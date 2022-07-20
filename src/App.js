1e42b1d761c1ced17f14b0446dd5cbdb75c877c5
Apple <apple@Apples-MacBook-Pro.local>
2022-07-20 21:53:59 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -7,30 +7,44 @@ import Person from "./Person";
 
 function App() {
   const Api = "https://quote-garden.herokuapp.com/api/v3/quotes/random";
-  const [loading ,setLoading] = useState(true);
+  const [loading ,setLoading] = useState(false);
   // const [message ,setMessage] = useState("Random");
   const[data,setData] = useState([{
-    quoteText:"",
-    quoteAuthor:"",
-    quoteGenre:""
+    quoteText:"The greatest glory in living lies not in never falling, but in rising every time we fall",
+    quoteAuthor:"Nelson Mandela",
+    quoteGenre:"life"
   }]);
+
+
  
- const random = async () => {
-  const response = await fetch(Api);
-  const result = await response.json();
-  setData(result.data);
-  // setMessage(result.message);
-  setLoading(!false);
-};
+//  const random = async () => {
+//   const response = await fetch(Api);
+//   const result = await response.json();
+//   setData(result.data);
+//   // setMessage(result.message);
+//   setLoading(!false);
+// };
+
+const random = () => {
+  setLoading(true);
+  fetch(Api)
+  .then((response) => response.json())
+  .then(result => {
+       setData(result.data);
+       setLoading(false);
+  })
+}
 
     
   return (
      <div >
     <Header  random = {random}  title = 'random'/>
-    <div className="container">
-    {loading ? <h1>Click random to see quote</h1> : <Card quoteText = {data[0].quoteText}/>}
-    <Person quoteGenre ={data[0].quoteGenre}  quoteAuthor = {data[0].quoteAuthor}/>
-    </div>
+     
+     
+    {loading ? <div className="loading"><p>Loading Please wait ... </p></div>  : <div className="container">
+    <Card quoteText = {data[0].quoteText}/>
+    <Person quoteGenre ={data[0].quoteGenre} quoteAuthor = {data[0].quoteAuthor}/>
+    </div> }
     <Footer/>
     </div>
   )
